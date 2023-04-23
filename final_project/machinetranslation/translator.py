c1543ec7561d24bbaddcc8e6d9f524357479346a
Mircea N <nunu.mircea@gmail.com>
2023-04-23 06:18:55 -0400

    step1
    Signed-off-by: Mircea N <nunu.mircea@gmail.com>

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,19 @@
+import json
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+version = os.environ['version']
+
+authenticator = IAMAuthenticator('{apikey}')
+language_translator = LanguageTranslatorV3(
+    version='{version}',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url('{url}')
